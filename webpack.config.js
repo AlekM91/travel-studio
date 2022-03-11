2e31e9abc5bdb2924d57592cb8b61c8af0308bf9
Aleksandar Mitrevski <aleksandarmitrevski91@gmail.com>
2022-03-11 12:31:33 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy Images', function() {
+            fse.copySync('./src/assets/images', './dist/assets/images')
+        })
+    }
+}
+
+// 1 page only: plugins [new HtmlWebpackPlugin({filename: 'index.html', template: './src/index.html'})]
+const pages = fse.readdirSync('./src').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({filename: page, template: `./src/${page}`})
+})
+
+const config = {
+    entry: {
+        main: path.resolve(__dirname, 'src/scripts/main.js')
+    },
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, 'src'),
+    },
+    
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, 'src'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+
+    mode: "development",
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "autoprefixer"
+                                    ]
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    // plugins
+    plugins: pages
+}
+
+if(currentTask == "build") {
+    config.output = {
+        filename: "[name].[chunkhash].js",
+        chunkFilename: "[name].[chunkhash].js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    }
+    config.mode = "production"
+    config.optimization = {
+        splitChunks: {chunks: "all"},
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    }
+    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+    config.plugins.push(
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new WebpackManifestPlugin(),
+        new RunAfterCompile()
+    )
+}
+
+module.exports = config;
